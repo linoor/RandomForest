26ec04f51edfca498c75116bdf74fd13cd1b0ff7
linoor <michal.pomaranski@gmail.com>
2014-11-07 16:46:46 +0100

    added two more tests

--- tests/TreeNodeTests.java
+++ tests/TreeNodeTests.java
@@ -1,17 +1,35 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.matchers.JUnitMatchers.*;
+
+import static org.hamcrest.CoreMatchers.hasItems;
 
 /**
  * Created by Linoor on 2014-11-07.
  */
 public class TreeNodeTests {
 
+    private TreeNode parent;
+    private TreeNode rightChild;
+    private TreeNode leftChild;
+
+    @Before
+    public void setup() {
+        parent = new TreeNode();
+        leftChild = new TreeNode();
+        parent.setLeftChild(leftChild);
+        rightChild = new TreeNode();
+        parent.setRightChild(rightChild);
+    }
+
+    @Test
+    public void testAddingChildren() {
+        org.junit.Assert.assertNotNull("left child should not be null", parent.getLeftChild());
+        org.junit.Assert.assertNotNull("right child should not be null", parent.getRightChild());
+    }
+
     @Test
     public void testIsLeaf() {
-        TreeNode parent;
-        parent = new TreeNode();
-        parent.setLeftChild(new TreeNode());
-        parent.setRightChild(new TreeNode());
-
         org.junit.Assert.assertFalse("parent should not be a leaf", parent.isLeaf());
         org.junit.Assert.assertTrue("left child should be a leaf", parent.getLeftChild().isLeaf());
         org.junit.Assert.assertTrue("right child should be a leaf", parent.getRightChild().isLeaf());
@@ -19,13 +37,14 @@ public class TreeNodeTests {
 
     @Test
     public void testLevel() {
-        TreeNode parent;
-        parent = new TreeNode();
-        parent.setLeftChild(new TreeNode());
-        parent.setRightChild(new TreeNode());
-
         org.junit.Assert.assertSame("The level of the parent should be equal to 0", 0, parent.getLevel());
         org.junit.Assert.assertSame("The level of the left child should be equal to 1", 1, parent.getLevel());
         org.junit.Assert.assertSame("The level of the right child should be equal to 1", 1, parent.getLevel());
+    }
+
+    @Test
+    public void testGetParent() {
+        org.junit.Assert.assertSame("left child's parent should be \"parent\"", parent, leftChild.getParent());
+        org.junit.Assert.assertSame("right child's parent should be \"parent\"", parent, leftChild.getParent());
     }
 }
