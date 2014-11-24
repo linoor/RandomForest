c97608c4d331e8f3ba2ae85c0d3b6c8ab88fd085
eric.chiu <wf870125@gmail.com>
2014-11-24 10:22:23 +0100

    treenode constructor

--- src/RandomForestHOG/DecisionTree/DecisionTree.java
+++ src/RandomForestHOG/DecisionTree/DecisionTree.java
@@ -2,69 +2,70 @@ package RandomForestHOG.DecisionTree;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("61261918-d6ad-4d4d-a19f-e6c7088f5dd6")
+// @objid ("61261918-d6ad-4d4d-a19f-e6c7088f5dd6")
 public class DecisionTree  {
-    @objid("ebb8f3f9-dd89-4d5b-b771-a57032b21977")
+    // @objid("ebb8f3f9-dd89-4d5b-b771-a57032b21977")
     int dataN() {
         return 0;
+        
     }
 
-    @objid("0e6e5a8a-cb9a-426f-99dd-bbc8cd785220")
+    // @objid("0e6e5a8a-cb9a-426f-99dd-bbc8cd785220")
     int trainN() {
         return 0;
     }
 
-    @objid("c8d51bcf-a123-439a-a3af-27758281545f")
+    // @objid("c8d51bcf-a123-439a-a3af-27758281545f")
     int attrN() {
         return 0;
     }
 
-    @objid("bd717090-214d-4987-9ab8-8616e6eb28ed")
+    // @objid("bd717090-214d-4987-9ab8-8616e6eb28ed")
     TreeNode rootNode() {
         return null;
     }
 
-    @objid ("92a699ab-b86a-40cc-8fdb-4eca568fa8a6")
+    // @objid ("92a699ab-b86a-40cc-8fdb-4eca568fa8a6")
     public DecisionTree() {
     }
 
-    @objid ("95f01270-0b39-4c6b-bbf3-fb177f21545e")
+    // @objid ("95f01270-0b39-4c6b-bbf3-fb177f21545e")
     public DecisionTree(final List<Float> data) {
     }
 
-    @objid ("bd35c418-14d7-4599-891b-34837487a39c")
+    // @objid ("bd35c418-14d7-4599-891b-34837487a39c")
     private void bootStrapSample(final List<Float> data, List<Float> train, List<Float> test) {
     }
 
-    @objid ("22963c8e-9140-49f2-beb7-3b2458a06c51")
+    // @objid ("22963c8e-9140-49f2-beb7-3b2458a06c51")
     private List<Integer> bootStrapAttr() {
         // TODO Auto-generated return
         return new ArrayList<Integer>();
     }
 
-    @objid ("11f42db2-137b-4fd3-8d5c-065ee3ecdf65")
+    // @objid ("11f42db2-137b-4fd3-8d5c-065ee3ecdf65")
     private void createTree(final List<Float> train, final int nTree) {
     }
 
-    @objid ("008d3f40-e60d-4c6e-9eb2-7018b83bf180")
+    // @objid ("008d3f40-e60d-4c6e-9eb2-7018b83bf180")
     private void recursiveSplit(final TreeNode parent) {
     }
 
-    @objid ("943639d0-f911-4e72-b5b3-3087f8f11863")
+    // @objid ("943639d0-f911-4e72-b5b3-3087f8f11863")
     public List<Integer> classify(final List<Float> testData) {
         // TODO Auto-generated return
         return new ArrayList<Integer>();
     }
 
-    @objid ("76d43c96-c471-4b7d-a417-d0a16d9c295c")
+    // @objid ("76d43c96-c471-4b7d-a417-d0a16d9c295c")
     public String saveToString() {
         // TODO Auto-generated return
         return null;
     }
 
-    @objid ("359fef83-1f91-4226-b4e6-33118900cb50")
+    // @objid ("359fef83-1f91-4226-b4e6-33118900cb50")
     public void loadFromString(final String tree) {
     }
 
--- src/RandomForestHOG/DecisionTree/TreeNode.java
+++ src/RandomForestHOG/DecisionTree/TreeNode.java
@@ -1,53 +1,49 @@
 package RandomForestHOG.DecisionTree;
 
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
+//import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 import java.util.ArrayList;
 
-@objid ("67321fdf-07c5-4b25-973e-2c0c213fa851")
+////@objid ("67321fdf-07c5-4b25-973e-2c0c213fa851")
 public class TreeNode {
+	private int level;
     private TreeNode leftChild;
     private TreeNode rightChild;
     private TreeNode parent;
-    private int level;
     private ArrayList<Double> data;
 
-    @objid ("51f070ca-6fe8-4d4e-868c-c62c2b31c082")
+    public boolean isLeaf;
+    public int splitAttr;
+    public double splitVal;
+    
+//    //@objid ("51f070ca-6fe8-4d4e-868c-c62c2b31c082")
     public TreeNode() {
+    	isLeaf = false;
         level = 0;
+        splitAttr = -99;
+        splitVal = -99;
     }
-
-    @objid("2299517a-7564-4108-ab5c-71ec87dc0b77")
+    
     public boolean isLeaf() {
-        return parent != null;
+    	return isLeaf;
     }
 
-    @objid("eba4fa88-e515-4ffe-92c8-c4719ccedcf3")
+  //@objid("eba4fa88-e515-4ffe-92c8-c4719ccedcf3")
     public int getLevel() {
         return level;
     }
 
-    @objid("4f2aede1-a0e6-4376-8945-3f234d7973ed")
-    int splitAttr() {
-        return 0;
-    }
-
-    @objid("17f327ce-60bb-464b-89fd-8117dccb10a2")
-    float splitVal() {
-        return 0;
-    }
-
-    @objid("d790bcf1-f79e-4f49-be58-cb0de2cd71fe")
+    //@objid("d790bcf1-f79e-4f49-be58-cb0de2cd71fe")
     public TreeNode getLeftChild() {
         return leftChild;
     }
 
-    @objid("f49884cb-a796-455c-89d2-3049637325cd")
+    //@objid("f49884cb-a796-455c-89d2-3049637325cd")
     public TreeNode getRightChild() {
         return rightChild;
     }
 
-    @objid("f30ffbce-5ef9-47c6-9c47-d8546919c4db")
+    //@objid("f30ffbce-5ef9-47c6-9c47-d8546919c4db")
     public TreeNode getParent() {
         return parent;
     }
