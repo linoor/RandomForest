36c213b5c8bd6aafc426447541e9ef61630e442f
Michał Pomarański <michal.pomaranski@gmail.com>
2014-11-24 08:45:04 +0100

    rectangular blockType test

--- tests/HOG/HOGTests.java
+++ tests/HOG/HOGTests.java
@@ -9,6 +9,8 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
 
+import static RandomForestHOG.HOG.HOGParam.BlockType.RADIAL;
+import static RandomForestHOG.HOG.HOGParam.BlockType.RECTANGULAR;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
@@ -18,31 +20,45 @@ import static org.junit.Assert.*;
 public class HOGTests {
 
     private TemporaryFolder tmpFolder;
+    private File tmpFile;
 
     @Before
     public void setup() {
         tmpFolder = new TemporaryFolder();
+        try {
+            tmpFile = tmpFolder.newFile("input.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testCreation() {
+        HOG hog = null;
         try {
-            File tmpFile = tmpFolder.newFile("input.txt");
-            HOG hog = new HOG(new HOGParam(HOGParam.BlockType.RADIAL, 10, 2, 4, 10, 5, 1), tmpFile);
+            hog = new HOG(new HOGParam(RADIAL, 10, 2, 4, 10, 5, 1), tmpFile);
             assertNotNull(hog);
-            assertThat("blockType should be radial", hog.getBlockType(), equalTo(HOGParam.BlockType.RADIAL));
+            assertThat("blockType should be radial", hog.getBlockType(), equalTo(RADIAL));
             assertEquals(hog.getBinNumber(), 10);
             assertEquals(hog.getCellWidth(), 2);
             assertEquals(hog.getCellHeight(), 4);
             assertEquals(hog.getBlockWidth(), 10);
             assertEquals(hog.getBlockHeight(), 5);
             assertEquals(hog.getMaskType(), 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-            fail();
         } catch (Exception e) {
             e.printStackTrace();
             fail();
+        }
+    }
+
+    @Test
+    public void testCreationRectangular() {
+        HOG hog;
+        try {
+            hog = new HOG(new HOGParam(RECTANGULAR, 10, 2, 4, 10, 5, 1), tmpFile);
+            assertThat("blockType should be rectangular", hog.getBlockType(), equalTo(RECTANGULAR));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
