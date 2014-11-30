36684711d983a1fb09e76050d7bef80de0be6a45
Michał Pomarański <michal.pomaranski@gmail.com>
2014-11-30 09:40:55 +0100

    turning on creation tests

--- tests/HOG/HOGTests.java
+++ tests/HOG/HOGTests.java
@@ -8,7 +8,6 @@ import RandomForestHOG.HOG.HOGParam;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Paths;
 
 import static RandomForestHOG.HOG.HOGParam.BlockType.RADIAL;
@@ -18,53 +17,42 @@ import static org.junit.Assert.*;
 
 public class HOGTests {
 
-    private TemporaryFolder tmpFolder;
-    private File tmpFile;
     private HOG exampleHOG;
 
-//    @Before
-//    public void setup() {
-//        tmpFolder = new TemporaryFolder();
-//        try {
-//            tmpFile = tmpFolder.newFile("input.png");
-//            exampleHOG = new HOG(new HOGParam(RECTANGULAR, 10, 2, 4, 10, 5, 1, 3, 3), tmpFile);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    public void testCreation() {
-//        HOG hog = null;
-//        try {
-//            hog = new HOG(new HOGParam(RADIAL, 10, 2, 4, 10, 5, 1, 3, 3), tmpFile);
-//            assertNotNull(hog);
-//            assertThat("blockType should be radial", hog.getBlockType(), equalTo(RADIAL));
-//            assertEquals(hog.getBinNumber(), 10);
-//            assertEquals(hog.getCellWidth(), 2);
-//            assertEquals(hog.getCellHeight(), 4);
-//            assertEquals(hog.getBlockWidth(), 10);
-//            assertEquals(hog.getBlockHeight(), 5);
-//            assertEquals(hog.getMaskType(), 1);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            fail();
-//        }
-//    }
-//
-//    @Test
-//    public void testCreationRectangular() {
-//        HOG hog;
-//        try {
-//            hog = new HOG(new HOGParam(RECTANGULAR, 10, 2, 4, 10, 5, 1, 3, 3), tmpFile);
-//            assertThat("blockType should be rectangular", hog.getBlockType(), equalTo(RECTANGULAR));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            fail();
-//        }
-//    }
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void testCreation() {
+        HOG hog = null;
+        try {
+            hog = new HOG(new HOGParam(RADIAL, 10, 2, 4, 10, 5, 1, 3, 3), Paths.get("E:\\Dev\\RandomForest\\assets\\vectorGradientTest.png").toFile());
+            assertNotNull(hog);
+            assertThat("blockType should be radial", hog.getBlockType(), equalTo(RADIAL));
+            assertEquals(hog.getBinNumber(), 10);
+            assertEquals(hog.getCellWidth(), 2);
+            assertEquals(hog.getCellHeight(), 4);
+            assertEquals(hog.getBlockWidth(), 10);
+            assertEquals(hog.getBlockHeight(), 5);
+            assertEquals(hog.getMaskType(), 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testCreationRectangular() {
+        HOG hog;
+        try {
+            hog = new HOG(new HOGParam(RECTANGULAR, 10, 2, 4, 10, 5, 1, 3, 3), Paths.get("E:\\Dev\\RandomForest\\assets\\vectorGradientTest.png").toFile());
+            assertThat("blockType should be rectangular", hog.getBlockType(), equalTo(RECTANGULAR));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
 
     @Test
     public void testGetPixelArray() {
