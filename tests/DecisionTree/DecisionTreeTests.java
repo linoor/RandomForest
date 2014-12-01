73621c9d02db80a5efd4a0f2a7d14625148223c7
eric.chiu <wf870125@gmail.com>
2014-12-02 00:20:57 +0100

    complete recursiveSplit (w/o test)

---
+++ tests/DecisionTree/DecisionTreeTests.java
@@ -0,0 +1,27 @@
+import RandomForestHOG.DecisionTree.DecisionTree;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecisionTreeTests {
+    
+    private DecisionTree tree;
+    private List<List<Double>> testingData; 
+
+    public DecisionTreeTests(List<List<Double>> testingData) {
+        this.testingData = testingData;
+    }
+    
+    @Before
+    public void setup() {
+        tree = new DecisionTree(testingData, 10);
+
+    }
+    
+    @Test
+    public void testBootstrapSample() {
+        
+    }
+    
+    
+
+}
